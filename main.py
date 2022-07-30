aa490f4b193c61e3967afdce5ae554ca7b2b2066
Muhammad Omar <57843931+Omar1234247@users.noreply.github.com>
2022-07-30 20:34:20 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+pc_dict = {
+  "1": ["Intel Core i5-3470 @ 3.2 GHz", "AMD Radeon HD 6670", "8 GB DDR4 RAM", "128 GB SSD"],
+  "2": ["Intel Core i5-3470 @ 3.2 GHz", "NVIDIA Quadro T400", "16 GB DDR4 RAM", "128 GB SSD"],
+  "3": ["AMD Ryzen 5000 series", "NVIDIA RTX 3090 Ti", "32 GB DDR5 RAM", "2 TB SSD"]
+}
+
+# although i barely have any money at all, i will try to save up for a new computer.
